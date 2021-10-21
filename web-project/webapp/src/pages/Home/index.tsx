8688e3ca22cb701887eb36f3ce09a5c915df32c6
LeeUmpierre <umpierre2507@hotmail.com>
2021-10-21 19:27:12 -0300

    Aula 14-10-21

--- web-project/webapp/src/pages/Home/index.tsx
+++ web-project/webapp/src/pages/Home/index.tsx
@@ -1,18 +1,32 @@
-import { AppBar, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, InputAdornment, TextField, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Button, Card, CardActionArea, CardContent, CardMedia, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, InputAdornment, TextField, Toolbar, Tooltip, Typography } from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
 import SortIcon from '@material-ui/icons/Sort';
 import { useEffect, useState } from "react";
+import { ErrorType } from "../../App";
 import { Header } from "../../components/Header";
 import { useAuth } from "../../hooks/useAuth";
 import serverAPI from "../../services/serverAPI";
 
+import imgBgCard from '../../assets/img/card-background.png';
+
 import './style.scss';
+import { useHistory } from "react-router";
+import { Project } from "../../@types";
 
 export function Home() {
+    const history = useHistory();
 
     const { user, token } = useAuth();
 
+    const [error, setError] = useState({} as ErrorType);
+
     const [openDialog, setOpenDialog] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const [projects, setProjects] = useState<any[]>([] as any);
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
 
     useEffect(() => {
         //Atribui o token salvo na requisição
@@ -20,20 +34,61 @@ export function Home() {
 
         //Carrega os projetos do servidor 
         serverAPI.get('/projects')
-            .then((response) => {
-                console.log(response);
+            .then(result => {
+                setProjects(result.data);
             })
-            .catch((error) => {
+            .catch(error => {
                 console.log(error);
             })
     }, [])
 
     function handleNewProject() {
+        setName('');
+        setDescription('');
         setOpenDialog(true);
     }
-
     function handleCreateProject() {
 
+        //Reset de variáveis
+        setError({} as ErrorType);
+
+        //Validar se o campo name foi preenchido
+        if (name.trim() === '') {
+            setError({
+                type: 'invalid-name',
+                message: 'Campo obrigatório'
+            });
+
+            return;
+        }
+
+        //Coloco a aplicação em modo loading
+        setLoading(true);
+
+        //Alimento um project para enviar ao servidor
+        const project = {
+            name: name,
+            description: description,
+            owner: user
+        }
+
+        //Envio o novo projeto para o servidor
+        serverAPI.post('/projects', project)
+            .then(result => {
+                setProjects([...projects, result.data]);
+                setLoading(false);
+                setOpenDialog(false);
+            })
+            .catch(error => {
+                setError({
+                    type: 'exception',
+                    message: error.message
+                })
+            })
+    }
+
+    function handleOpenProject(project: Project): void {
+        history.push(`/projects/${project.id}`);
     }
 
     return (
@@ -76,7 +131,48 @@ export function Home() {
                     </AppBar>
                 
                     <div className="project-content">
+                        {projects.map(project => {
+                            return (
+                                <div
+                                    key={project.id}
+                                    className="project-card-container">
+                                    <Card
+                                        className="project-card">
+                                        <CardActionArea
+                                            onClick={() => handleOpenProject(project)}>
+                                            
+                                            <CardContent
+                                                className="project-card-title">
+                                                {project.name.length >= 28 ? (
 
+                                                    <Tooltip title={project.name}>
+                                                        <Typography variant="h6" component="span">
+                                                            {`${project.name.substring(0, 25)}...`}
+                                                        </Typography>   
+                                                    </Tooltip>
+
+                                                ) : (
+
+                                                    <Typography component="span">
+                                                        {project.name}
+                                                    </Typography>
+
+                                                ) }
+                                            </CardContent>
+
+                                            <CardMedia
+                                                className="project-card-media"
+                                                image={imgBgCard}/>
+                                        </CardActionArea>    
+                                    </Card> 
+
+                                    <Avatar
+                                        className="project-avatar"
+                                        src={`../assets/avatar/${project.owner.avatar}`} 
+                                        alt={project.owner.name}/>   
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </main>
@@ -98,38 +194,53 @@ export function Home() {
                     <TextField
                         type="text"
                         fullWidth
+                        autoFocus
                         label="Nome do Projeto"
                         variant="outlined"
                         size="small"
                         margin="normal"
+                        value={name}
+                        onChange={event => setName(event.target.value)}
+                        disabled={loading}
+                        error={error.type === 'invalid-name'}
+                        helperText={error.type === 'invalid-name' && error.message}
                     />
 
                     <TextField
                         type="text"
                         fullWidth
-                        label="Nome do Projeto"
+                        label="Descrição do Projeto"
                         variant="outlined"
                         size="small"
                         multiline
                         rows={4}
                         margin="normal"
+                        value={description}
+                        onChange={event => setDescription(event.target.value)}
+                        disabled={loading}
                     />
-
 
                 </DialogContent>
 
                 <DialogActions>
+                    
+                    {loading && <CircularProgress size={24} />}
+
                     <Button
                         variant="outlined"
-                        size="small"
-                        onClick={() => setOpenDialog(false)}>
+                        size="medium"
+                        onClick={() => setOpenDialog(false)}
+                        disabled={loading}
+                    >
                         Cancelar
                     </Button>
                     <Button
                         color="primary"
                         variant="contained"
-                        size="small"
-                        onClick={handleCreateProject}>
+                        size="medium"
+                        onClick={handleCreateProject}
+                        disabled={loading}
+                    >
                         Confirmar
                     </Button>
                 </DialogActions>
